efd94c364d38f0c687108ea0003ca85f461db5b3
Chunporo <hackingoxo@gmail.com>
2023-11-05 00:49:47 +0700

    Change something

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
 from flask import Flask, redirect, url_for, request, render_template
+import sqlite3
 from markupsafe import escape
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
-    return render_template('index.html');
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()  
+    conn.close()
+    return render_template('index.html', posts=posts)
 
-@app.route('/login')
-def login():
-    return 'login'
+# @app.route('/login')
+# def login():
+#     return 'login'
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)} profile'
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'User {escape(username)} profile'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'    
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'    
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
 
 # with app.test_request_context():
 #     print(url_for('index'))
 #     print(url_for('login'))
 #     print(url_for('login', next='/'))
 #     print(url_for('profile', username='John Doe'))
+
+@app.route('/image_augmentation')
+def image_augmentation():
+    return render_template('image_gen.html')
 
 if __name__ == '__main__':  
     app.run(debug=True)
